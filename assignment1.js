fe74a57adcc46a24e1d5d876707c35e7751c01f7
jasonzou1 <90172098+jasonzou1@users.noreply.github.com>
2023-05-20 19:32:32 -0400

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ___Jiasheng Zou___________________ Student ID: ___141462226___________ Date: ________May 20th 2023________
+*
+********************************************************************************/ 
 let serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
 let serverPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout"];
 let serverResponses = [
